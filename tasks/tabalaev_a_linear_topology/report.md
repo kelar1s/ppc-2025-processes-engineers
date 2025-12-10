efb496258e479c418f7b2e5aa01e59271be61856
kelar1s <atabalaev21@gmail.com>
2025-12-10 21:15:48 +0300

    rename to linear topology

---
+++ tasks/tabalaev_a_linear_topology/report.md
@@ -0,0 +1,13 @@
+# Линейка
+
+- Студент: Табалаев Антон Максимович
+- Технология: SEQ | MPI
+- Вариант: 6
+
+
+## 8. Источники
+
+1. Сысоев А. В. Лекции по курсу «Параллельное программирование для кластерных систем».
+2. Официальная документация Microsoft MPI — https://learn.microsoft.com/ru-ru/message-passing-interface
+3. Документация Open MPI — https://www.open-mpi.org/doc/
+4. C++ Reference — https://en.cppreference.com
---
+++ tasks/tabalaev_a_linear_topology/tests/performance/main.cpp
@@ -0,0 +1,55 @@
+#include <gtest/gtest.h>
+
+#include <cstddef>
+#include <tuple>
+#include <vector>
+
+#include "tabalaev_a_linear_topology/common/include/common.hpp"
+#include "tabalaev_a_linear_topology/mpi/include/ops_mpi.hpp"
+#include "tabalaev_a_linear_topology/seq/include/ops_seq.hpp"
+#include "util/include/perf_test_util.hpp"
+
+namespace tabalaev_a_linear_topology {
+
+class TabalaevALinearTopologyPerfTests : public ppc::util::BaseRunPerfTests<InType, OutType> {
+  void SetUp() override {
+    size_t rows = 5000;
+    size_t columns = 5000;
+    std::vector<int> matrix(rows * columns);
+    for (size_t i = 0; i < rows; i++) {
+      for (size_t j = 0; j < columns; j++) {
+        int index = static_cast<int>((i * columns) + j);
+        matrix[index] = static_cast<int>((i * i) + j);
+      }
+    }
+    matrix[(rows * columns) / 2] = expected_minik_;
+    input_data_ = std::make_tuple(rows, columns, matrix);
+  }
+
+  bool CheckTestOutputData(OutType &output_data) final {
+    return output_data == expected_minik_;
+  }
+
+  InType GetTestInputData() final {
+    return input_data_;
+  }
+
+ private:
+  InType input_data_;
+  int expected_minik_ = -1000000;
+};
+
+TEST_P(TabalaevALinearTopologyPerfTests, RunPerfModes) {
+  ExecuteTest(GetParam());
+}
+
+const auto kAllPerfTasks = ppc::util::MakeAllPerfTasks<InType, TabalaevALinearTopologyMPI, TabalaevALinearTopologySEQ>(
+    PPC_SETTINGS_tabalaev_a_linear_topology);
+
+const auto kGtestValues = ppc::util::TupleToGTestValues(kAllPerfTasks);
+
+const auto kPerfTestName = TabalaevALinearTopologyPerfTests::CustomPerfTestName;
+
+INSTANTIATE_TEST_SUITE_P(RunModeTests, TabalaevALinearTopologyPerfTests, kGtestValues, kPerfTestName);
+
+}  // namespace tabalaev_a_linear_topology
