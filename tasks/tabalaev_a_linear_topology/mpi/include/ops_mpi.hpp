9c78a3029504cf1590a8fd8647d043b4ad6b1ed6
kelar1s <atabalaev21@gmail.com>
2025-12-14 01:39:52 +0300

    fix clang-tidy issue 1.4

--- tasks/tabalaev_a_linear_topology/mpi/include/ops_mpi.hpp
+++ tasks/tabalaev_a_linear_topology/mpi/include/ops_mpi.hpp
@@ -20,13 +20,13 @@ class TabalaevALinearTopologyMPI : public BaseTask {
   bool RunImpl() override;
   bool PostProcessingImpl() override;
 
-  [[nodiscard]] int GetLeft(int rank);
-  [[nodiscard]] int GetRight(int rank, int size);
-  [[nodiscard]] int GetDirection(int sender, int receiver);
-  [[nodiscard]] bool IsOnPath(int rank, int sender, int receiver, int direction);
+  [[nodiscard]] static int GetLeft(int rank);
+  [[nodiscard]] static int GetRight(int rank, int size);
+  [[nodiscard]] static int GetDirection(int sender, int receiver);
+  [[nodiscard]] static bool IsOnPath(int rank, int sender, int receiver, int direction);
   void ProcessSender(int direction, int left, int right, std::vector<int> &local_buff);
-  void ProcessIntermediate(int direction, int left, int right, std::vector<int> &local_buff);
-  void ProcessReceiver(int direction, int left, int right, std::vector<int> &local_buff);
+  void static ProcessIntermediate(int direction, int left, int right, std::vector<int> &local_buff);
+  void static ProcessReceiver(int direction, int left, int right, std::vector<int> &local_buff);
 };
 
 }  // namespace tabalaev_a_linear_topology
